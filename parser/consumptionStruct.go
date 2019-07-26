e110a33faff3090fd4b4275cf2d0cfeabd4a5eaa
Marcos Tenrero <tenrero@aol.com>
2019-07-26 17:42:50 +0200

    golang Time parse method

--- parser/consumptionStruct.go
+++ parser/consumptionStruct.go
@@ -1,9 +1,16 @@
 package parser
 
+import "time"
+
 // ConsumptionEntry represents the data given by IDE Spanish Ligth distributor
 type ConsumptionEntry struct {
 	CUPS    string `json:"CUPS"`
 	Fecha   string `json:"Fecha"`
 	Hora    string `json:"Hora"`
 	Consumo string `json:"Consumo_kWh"`
+}
+
+func (entry *ConsumptionEntry) timeConsumption() (time.Time, error) {
+	timeStr := entry.Fecha + " " + entry.Hora
+	return time.Parse("02/01/2006 15", timeStr)
 }
--- parser/csv_test.go
+++ parser/csv_test.go
@@ -1,9 +1,12 @@
 package parser
 
-import "testing"
+import (
+	"testing"
+)
+
+var sampleCSV = []byte("CUPS;Fecha;Hora;Consumo_kWh;Metodo_obtencion\nES9234823489237489EE;16/05/2019;1;0,141;R\nES3282748273489237EA;16/05/2019;2;0,111;R")
 
 func TestCSVParse(t *testing.T) {
-	sampleCSV := []byte("CUPS;Fecha;Hora;Consumo_kWh;Metodo_obtencion\nES9234823489237489EE;16/05/2019;1;0,141;R\nES3282748273489237EA;16/05/2019;2;0,111;R")
 
 	var consumptionsParsed []ConsumptionEntry
 
@@ -27,4 +30,19 @@ func TestCSVParse(t *testing.T) {
 	if consumptionsParsed[1].Hora != "2" {
 		t.Error("Not parsed correctly")
 	}
+}
+
+func TestTimeParse(t *testing.T) {
+	consumptionsParsed, err := parseCSVBytes(sampleCSV)
+
+	sTime, err := consumptionsParsed[0].timeConsumption()
+
+	if err != nil {
+		t.Error("Error parsing time fields")
+	}
+
+	if sTime.Format("02 Jan 06 15:04") != "16 May 19 01:00" {
+		t.Error("Date not parsed correctly")
+	}
+
 }
