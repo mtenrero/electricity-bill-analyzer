213e3a56add1128eed761e7b1bd2350ef5ebf1b5
Marcos Tenrero <tenrero@aol.com>
2019-07-28 15:38:16 +0200

    hourly Analyzer

---
+++ analyzers/hourly.go
@@ -0,0 +1,29 @@
+package analyzers
+
+import (
+	"strconv"
+
+	"github.com/mtenrero/lightBillAnalyzer/parser"
+	"github.com/thoas/go-funk"
+	"gonum.org/v1/gonum/stat"
+)
+
+// ReportHourly makes calculations (Mean) by hour based on the electricity consumption
+func ReportHourly(consumptionsByHour *parser.ConsumptionsByHour) []float64 {
+
+	sumKwH := make([]float64, 24)
+
+	for hour, consumptions := range *consumptionsByHour {
+
+		listConsumptions := funk.Map(*consumptions, func(consumption parser.ConsumptionEntry) float64 {
+			float, _ := strconv.ParseFloat(consumption.Consumo, 64)
+			return float
+		}).([]float64)
+
+		hourMean := stat.Mean(listConsumptions, nil)
+
+		sumKwH[hour] = hourMean
+	}
+
+	return sumKwH
+}
---
+++ analyzers/hourly_test.go
@@ -0,0 +1,39 @@
+package analyzers
+
+import (
+	"testing"
+
+	"github.com/mtenrero/lightBillAnalyzer/parser"
+)
+
+func TestHourlyReport(t *testing.T) {
+
+	consumptions := make(parser.Consumptions, 0)
+	cons1 := parser.ConsumptionEntry{
+		Hora:    "3",
+		Consumo: "0.7",
+	}
+	cons2 := parser.ConsumptionEntry{
+		Hora:    "3",
+		Consumo: "3.8",
+	}
+	cons3 := parser.ConsumptionEntry{
+		Hora:    "3",
+		Consumo: "5",
+	}
+	consumptions = append(consumptions, cons1)
+	consumptions = append(consumptions, cons2)
+	consumptions = append(consumptions, cons3)
+
+	hourlyConsumptions := *parser.SplitConsumptionsHourly(&consumptions)
+
+	report := ReportHourly(&hourlyConsumptions)
+
+	if len(*hourlyConsumptions[3]) != 3 {
+		t.Fail()
+	}
+
+	if report[3] != 3.1666666666666665 {
+		t.Fail()
+	}
+}
