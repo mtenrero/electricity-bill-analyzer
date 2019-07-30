b274f199da63e69255e42ead07681d86bac652f5
Marcos Tenrero <marcostm@meta4.com>
2019-07-30 11:07:13 +0200

    fixed unexpected comma instead dot while parsing consumption

--- analyzers/hourly.go
+++ analyzers/hourly.go
@@ -2,6 +2,7 @@ package analyzers
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/mtenrero/electricity-bill-analyzer/parser"
 	"github.com/thoas/go-funk"
@@ -18,8 +19,9 @@ func ReportHourly(consumptions *parser.Consumptions) Report {
 	for hour, consumptions := range *consumptionsByHour {
 
 		listConsumptions := funk.Map(*consumptions, func(consumption parser.ConsumptionEntry) float64 {
-			float, _ := strconv.ParseFloat(consumption.Consumo, 64)
-			return float
+			consumoSanitized := strings.Replace(consumption.Consumo, ",", ".", -1)
+			floatValue, _ := strconv.ParseFloat(consumoSanitized, 64)
+			return floatValue
 		}).([]float64)
 
 		hourMean := stat.Mean(listConsumptions, nil)
