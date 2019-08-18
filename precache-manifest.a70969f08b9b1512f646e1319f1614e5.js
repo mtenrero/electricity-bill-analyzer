c5d3b0993b0c66e3ecc1187277e4cafbe089990b
Marcos Tenrero Moran <tenrero@aol.com>
2019-08-18 22:04:48 +0200

    Updates

---
+++ precache-manifest.a70969f08b9b1512f646e1319f1614e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff612dd49ae37859c20afdb79a6fb2b",
+    "url": "/electricity-bill-analyzer/index.html"
+  },
+  {
+    "revision": "081bfef2e895db8e2771",
+    "url": "/electricity-bill-analyzer/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "6a4fd86efc7c2f1fbaa6",
+    "url": "/electricity-bill-analyzer/static/css/main.7121833c.chunk.css"
+  },
+  {
+    "revision": "081bfef2e895db8e2771",
+    "url": "/electricity-bill-analyzer/static/js/2.7c9c8f92.chunk.js"
+  },
+  {
+    "revision": "6a4fd86efc7c2f1fbaa6",
+    "url": "/electricity-bill-analyzer/static/js/main.280de773.chunk.js"
+  },
+  {
+    "revision": "6388a6a0e0ddacb444e6",
+    "url": "/electricity-bill-analyzer/static/js/runtime~main.e799f711.js"
+  }
+]);
